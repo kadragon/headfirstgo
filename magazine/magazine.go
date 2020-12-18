ba1f80abd9644f7de835994889ff601e00e84938
kangdonguk <kangdongouk@gmail.com>
2020-12-18 15:12:38 +0900

    8장 끝

---
+++ magazine/magazine.go
@@ -0,0 +1,27 @@
+package magazine
+
+type Subscriber struct {
+	Name        string
+	Rate        float64
+	Active      bool
+	HomeAddress Address
+}
+
+type Employee struct {
+	Name        string
+	Salary      float64
+	HomeAddress Address
+}
+
+type Employee2 struct {
+	Name   string
+	Salary float64
+	Address
+}
+
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
+}
